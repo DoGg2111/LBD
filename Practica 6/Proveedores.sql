37ab5d980e2a481911a1a12aa95f809c063edfd8
DoGg2111 <47579906+DoGg2111@users.noreply.github.com>
2019-10-31 10:57:38 -0600

    Add files via upload

---
+++ Practica 6/Proveedores.sql
@@ -0,0 +1,135 @@
+use Farmacia
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (1, 'Gabtype', '7388434052', 'ksoulsby0@hp.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (2, 'Brightbean', '3142165051', 'mcrysell1@hibu.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (3, 'Fivespan', '9005014308', 'bjosephi2@tripod.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (4, 'Nlounge', '4903405006', 'dpittendreigh3@hud.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (5, 'Dablist', '8917215884', 'hbolderoe4@google.cn');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (6, 'Fliptune', '7615365388', 'fbuffham5@comsenz.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (7, 'Brainsphere', '6221659128', 'dpedracci6@yolasite.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (8, 'Feedfire', '3112885337', 'rbuller7@paginegialle.it');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (9, 'Gevee', '7836654358', 'eolech8@dell.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (10, 'Brainlounge', '5796138660', 'dprisley9@addthis.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (11, 'Thoughtbridge', '9908761338', 'cruppertza@vk.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (12, 'Shuffletag', '3836230239', 'jwindasb@house.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (13, 'Jaxbean', '7053025676', 'vzemlerc@sohu.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (14, 'Dabjam', '6572976467', 'eashleed@miibeian.gov.cn');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (15, 'Podcat', '5777904811', 'lmitchinere@hao123.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (16, 'Podcat', '8526357787', 'uwhorltonf@bravesites.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (17, 'Twitterlist', '5997204998', 'kquadlingg@usda.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (18, 'Voomm', '6293524488', 'dyepiskopovh@usgs.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (19, 'Skipstorm', '2229485323', 'ahowbrooki@list-manage.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (20, 'Meevee', '6119120081', 'pfaersj@rediff.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (21, 'Meejo', '6174558220', 'lsimmgenk@webmd.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (22, 'Tagcat', '4348546786', 'chambridgel@discuz.net');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (23, 'Linklinks', '3158287363', 'dboorm@istockphoto.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (24, 'Jabberstorm', '4051595025', 'akurtonn@sphinn.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (25, 'Rhynyx', '9076307608', 'kmayzeo@example.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (26, 'Linkbridge', '3477117888', 'mredfearnp@dagondesign.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (27, 'Thoughtstorm', '5174535036', 'eradboneq@salon.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (28, 'Yakijo', '4481821043', 'asothamr@quantcast.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (29, 'Yamia', '8635674743', 'dbalasinis@prweb.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (30, 'Zoomcast', '7251392598', 'lkubackit@nature.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (31, 'DabZ', '2796465468', 'rglassu@histats.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (32, 'Ntag', '7828464425', 'aedenv@accuweather.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (33, 'Jetpulse', '4863673209', 'fcostockw@usda.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (34, 'Innojam', '8517228305', 'mmailex@dagondesign.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (35, 'Oyoba', '6983857317', 'ihallasy@wunderground.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (36, 'Photospace', '6889946042', 'cduncanz@indiegogo.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (37, 'Zazio', '4834324484', 'bplott10@ameblo.jp');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (38, 'Livepath', '6213745823', 'mlepper11@people.com.cn');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (39, 'Lazzy', '2857657333', 'kmcgrale12@fotki.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (40, 'Photobean', '9756886943', 'rbendle13@arstechnica.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (41, 'Avamba', '8834714281', 'clinney14@discovery.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (42, 'Ozu', '5513211175', 'kjennaroy15@nymag.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (43, 'Bluezoom', '4535015572', 'tmaso16@squarespace.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (44, 'Fatz', '6885815617', 'rbrimblecomb17@theatlantic.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (45, 'Fatz', '2822527551', 'vsier18@live.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (46, 'Dabfeed', '5128343905', 'ashutler19@ezinearticles.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (47, 'Twimbo', '6376287070', 'hvoase1a@lulu.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (48, 'Skyndu', '5716082923', 'vmalyan1b@weebly.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (49, 'Topicshots', '8647808905', 'adelaney1c@shareasale.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (50, 'Realblab', '3754214294', 'sflade1d@vk.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (51, 'Linktype', '3042700331', 'fshorrock1e@bluehost.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (52, 'Realcube', '2727440022', 'gslesser1f@nature.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (53, 'Flashset', '5742881548', 'slukesch1g@shinystat.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (54, 'Yodoo', '4561322867', 'uquenby1h@salon.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (55, 'Bluejam', '2021034178', 'wjedrzejczyk1i@bloglines.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (56, 'Tambee', '2022858676', 'rpedlow1j@nymag.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (57, 'Gabspot', '5348272069', 'jkinner1k@telegraph.co.uk');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (58, 'Linklinks', '6206966291', 'lyarnton1l@marketwatch.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (59, 'Zoombox', '2097746912', 'gcarillo1m@nhs.uk');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (60, 'Gabtype', '5921162909', 'bhaddeston1n@ca.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (61, 'Twinder', '6044118032', 'fhealy1o@xing.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (62, 'Realbridge', '4916807859', 'ldrewitt1p@feedburner.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (63, 'Thoughtworks', '3223095488', 'mcrackel1q@hud.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (64, 'Plajo', '2037216330', 'aashall1r@qq.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (65, 'Skyndu', '5718362268', 'dbrockington1s@typepad.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (66, 'Voonix', '9419876069', 'agiddens1t@wp.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (67, 'Jaxbean', '3107378515', 'mnyssens1u@psu.edu');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (68, 'Blogspan', '3589830341', 'emerryman1v@microsoft.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (69, 'Yata', '9182521305', 'rfagan1w@imdb.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (70, 'InnoZ', '1277687821', 'ldehooge1x@baidu.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (71, 'Katz', '7287167251', 'sjeune1y@buzzfeed.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (72, 'Vinder', '4801726972', 'cculvey1z@reddit.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (73, 'Skinder', '2298539230', 'pnunson20@amazon.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (74, 'Brainsphere', '9453294535', 'dlyven21@networksolutions.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (75, 'Yodo', '2898859540', 'btaberner22@goodreads.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (76, 'Photobug', '7039974188', 'lprestie23@irs.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (77, 'Zoomdog', '6811465403', 'bemmanueli24@hc360.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (78, 'Voonte', '8741585657', 'bstolle25@tripadvisor.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (79, 'Flipbug', '4026726899', 'acomusso26@earthlink.net');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (80, 'Brainlounge', '6559915764', 'kbevir27@cbsnews.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (81, 'Roomm', '2662097249', 'asnaddin28@cbsnews.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (82, 'Dabvine', '2896496383', 'trivalant29@fotki.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (83, 'Mymm', '4996612134', 'rrebichon2a@psu.edu');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (84, 'Edgetag', '4832497884', 'pduffy2b@yolasite.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (85, 'Centidel', '1674869616', 'bdaddow2c@scribd.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (86, 'Skimia', '7018992876', 'ghuxley2d@washingtonpost.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (87, 'Janyx', '6667985100', 'lbarlthrop2e@bbb.org');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (88, 'Vimbo', '5784564965', 'kmaryott2f@salon.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (89, 'Rhybox', '3651518517', 'tkurt2g@utexas.edu');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (90, 'Dabfeed', '4577072284', 'erippin2h@mapquest.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (91, 'Feednation', '3658280828', 'nwyett2i@mapy.cz');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (92, 'Riffpedia', '5005880128', 'kflohard2j@state.gov');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (93, 'Demimbu', '9816241392', 'clulham2k@about.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (94, 'Livetube', '9364162845', 'rsudron2l@shareasale.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (95, 'Vipe', '3385571343', 'phosburn2m@blog.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (96, 'Meetz', '3093517296', 'drodda2n@delicious.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (97, 'Photospace', '7044341673', 'cbickers2o@opera.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (98, 'Tagopia', '5758143134', 'bwoloschinski2p@dailymotion.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (106, 'Fivespan', '3675491192', 'tmachon2q@meetup.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (100, 'Zoomzone', '7573981931', 'oaddy2r@cloudflare.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (101, 'Eabox', '9053537463', 'eklus2s@over-blog.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (102, 'Bubbletube', '9399042936', 'brisman2t@merriam-webster.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (103, 'Divavu', '7594366920', 'cborell2u@globo.com');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (104, 'Photospace', '8162623696', 'vgrolmann2v@army.mil');
+insert into Proveedores (id_proveedor, nombre_prov, telefono_prov, correo_prov) values (105, 'Devify', '3234870998', 'pdowsett2w@linkedin.com');
+
+DELETE FROM Proveedores
+WHERE nombre_prov = 'Vipe';
+DELETE FROM Proveedores
+WHERE nombre_prov = 'Eabox';
+DELETE FROM Proveedores
+WHERE telefono_prov = '3675491192';
+DELETE FROM Proveedores
+WHERE id_proveedor = '55';
+DELETE FROM Proveedores
+WHERE correo_prov = 'cborell2u@globo.com';
+
+UPDATE Proveedores
+SET nombre_prov = 'demsa'
+WHERE id_proveedor = 1;
+UPDATE Proveedores
+SET nombre_prov = 'cobisa'
+WHERE id_proveedor = 2;
+UPDATE Proveedores
+SET nombre_prov = 'Farmacia Los Angeles'
+WHERE id_proveedor = 3;
+UPDATE Proveedores
+SET nombre_prov = 'difarrey'
+WHERE id_proveedor = 4;
+UPDATE Proveedores
+SET nombre_prov = 'Probemedic'
+WHERE id_proveedor = 5;
+
+select * from Proveedores
